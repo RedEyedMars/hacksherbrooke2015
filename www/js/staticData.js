7f038ee4673da9427c1aa6b0d9e90ea8a8340490
kylegill <kyle.n.gill@gmail.com>
2015-04-25 12:59:09 -0400

    Split static data + update UI

---
+++ www/js/staticData.js
@@ -0,0 +1,40 @@
+var mainNavdata = [
+	{
+		"text" : "Contact us",
+		"url" : "http://www.ville.sherbrooke.qc.ca/en/directory-of-services/"
+	},{
+		"text" : "Site map",
+		"url" : "http://www.ville.sherbrooke.qc.ca/en/menus/liens-rapides-haut/site-map/"
+	},{
+		"text" : "Pressroom",
+		"url" : "http://www.ville.sherbrooke.qc.ca/en/pressroom/"
+	},{
+		"text" : "Fr",
+		"url" : "http://www.ville.sherbrooke.qc.ca/fr/"
+	}
+];
+	
+var sideNavData = [
+	{
+		"text" : "Data Visualization 1",
+		"Action" : "dataVis1"
+	},{
+		"text" : "Data Visualization 2",
+		"Action" : "dataVis2"
+	},{
+		"text" : "Data Visualization 3",
+		"Action" : "dataVis3"
+	},{
+		"text" : "Data Visualization 4",
+		"Action" : "dataVis4"
+	},{
+		"text" : "Data Visualization 5",
+		"Action" : "dataVis5"
+	},{
+		"text" : "Data Visualization 6",
+		"Action" : "dataVis6"
+	},{
+		"text" : "Data Visualization 7",
+		"Action" : "dataVis7"
+	}
+];
--- www/js/viewModel.js
+++ www/js/viewModel.js
@@ -1,25 +1,11 @@
-var viewModel = function(){
+var viewModel = function(mainNavdata, sideNavdata){
 	var self = this;
 
 	self.mainNav = ko.observableArray(mainNavdata);
+	self.sideNav = ko.observableArray(sideNavData);
+
+	self.changeContext = function(navItem, navObj, evt){
+
+	}
 }
 
-var mainNavdata = [
-	{
-		"text" : "My favorites",
-		"url" : "http://www.ville.sherbrooke.qc.ca/#"
-	},{
-		"text" : "Contact us",
-		"url" : "http://www.ville.sherbrooke.qc.ca/en/directory-of-services/"
-	},{
-		"text" : "Site map",
-		"url" : "http://www.ville.sherbrooke.qc.ca/en/menus/liens-rapides-haut/site-map/"
-	},{
-		"text" : "Pressroom",
-		"url" : "http://www.ville.sherbrooke.qc.ca/en/pressroom/"
-	},{
-		"text" : "Francais",
-		"url" : "http://www.ville.sherbrooke.qc.ca/fr/"
-	}
-];
-	
