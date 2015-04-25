475c1347f508c726a97b1c0c9b910f23d666b2b4
kylegill <kyle.n.gill@gmail.com>
2015-04-25 11:31:17 -0400

    Front-end updates + D3 fix

--- www/js/viewModel.js
+++ www/js/viewModel.js
@@ -1,5 +1,25 @@
 var viewModel = function(){
 	var self = this;
 
-
+	self.mainNav = ko.observableArray(mainNavdata);
 }
+
+var mainNavdata = [
+	{
+		"text" : "My favorites",
+		"url" : "http://www.ville.sherbrooke.qc.ca/#"
+	},{
+		"text" : "Contact us",
+		"url" : "http://www.ville.sherbrooke.qc.ca/en/directory-of-services/"
+	},{
+		"text" : "Site map",
+		"url" : "http://www.ville.sherbrooke.qc.ca/en/menus/liens-rapides-haut/site-map/"
+	},{
+		"text" : "Pressroom",
+		"url" : "http://www.ville.sherbrooke.qc.ca/en/pressroom/"
+	},{
+		"text" : "Francais",
+		"url" : "http://www.ville.sherbrooke.qc.ca/fr/"
+	}
+];
+	
